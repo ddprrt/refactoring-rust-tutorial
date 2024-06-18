29ed40b2fe4d1d474bcf4b720cb2bb4ae219f655
Stefan Baumgartner <sbaumg@gmail.com>
2024-06-18 13:10:34 +0200

    cleanup

--- tests/lesson-2.rs
+++ tests/lesson-2.rs
@@ -16,13 +16,18 @@ async fn basic_db_test() {
             Request::builder()
                 .uri("/kv/test")
                 .method("POST")
+                .header("content-type", "text/plain")
                 .body("Hello World".into())
                 .unwrap(),
         )
         .await
         .unwrap();
 
-    assert_eq!(response.status(), StatusCode::OK);
+    //assert_eq!(response.status(), StatusCode::OK);
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    let bytes = body.to_vec();
+    assert_eq!(String::from_utf8(bytes).unwrap(), "OK".to_string());
 
     let response = app
         .call(
@@ -36,21 +41,22 @@ async fn basic_db_test() {
         .unwrap();
 
     let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    assert_eq!(&body[..], b"Hello World");
+    let bytes = body.to_vec();
+    assert_eq!(String::from_utf8(bytes).unwrap(), "Hello World".to_string());
 }
 
-#[ignore]
 #[tokio::test]
-async fn big_request() {
+async fn image_request() {
     let state = SharedState::default();
     let mut app = router(&state);
-    let bytes = include_bytes!("../crab.png");
+    let bytes = include_bytes!("../crab-small.png");
 
     let response = app
         .call(
             Request::builder()
                 .uri("/kv/crab")
                 .method("POST")
+                .header("content-type", "image/png")
                 .body(bytes[..].into())
                 .unwrap(),
         )
@@ -92,4 +98,75 @@ async fn no_entry() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
+}
+
+#[tokio::test]
+async fn grayscale_request() {
+    let state = SharedState::default();
+    let mut app = router(&state);
+    let bytes = include_bytes!("../crab-small.png");
+    let grayscale = include_bytes!("../crab-small-grayscale.png");
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/crab")
+                .method("POST")
+                .header("content-type", "image/png")
+                .body(bytes[..].into())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/crab/grayscale")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(&body[..], &grayscale[..]);
+}
+
+#[tokio::test]
+async fn grayscale_faulty_request() {
+    let state = SharedState::default();
+    let mut app = router(&state);
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/test")
+                .method("POST")
+                .header("content-type", "text/plain")
+                .body("Hello World".into())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/test/grayscale")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::FORBIDDEN);
 }
